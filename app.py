fd306866f30f18d4030a214d6fd46ab4e4193c4d
kpant2190 <100390825+kpant2190@users.noreply.github.com>
2025-06-05 15:50:29 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from langchain_core.output_parsers import StrOutputParser
 
 import streamlit as st
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 ## Langmith tracking
